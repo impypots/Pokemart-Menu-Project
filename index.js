74ec636589512ef9b55133721c6c0721f5cbac1e
impypots <72578093+impypots@users.noreply.github.com>
2021-01-08 22:43:08 -0700

    Barebones start to project

---
+++ index.js
@@ -0,0 +1,17 @@
+let inventory = {
+    "HYPER POTION": 0,
+    "SUPER POTION": 0,
+    "FULL HEAL": 0,
+    "REVIVE": 0,
+    "ANTIDOTE": 0,
+    "BURN HEAL": 0,
+    "ICE HEAL": 0,
+    "AWAKENING": 0,
+    "PARLYZ HEAL": 0
+}
+
+let money = 12000
+
+document.getElementById("test").innerText = `HYPER POTION x ${inventory["HYPER POTION"]}`
+
+console.log(inventory)
